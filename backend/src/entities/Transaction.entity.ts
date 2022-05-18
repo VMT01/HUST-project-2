fbaae032675584c253f09495cec373d7eb832d0d
Vũ Minh Thanh <vuminhthanh8501@gmail.com>
2022-05-18 16:03:37 +0700

    Fix db naming convention

--- backend/src/entities/Transaction.entity.ts
+++ backend/src/entities/Transaction.entity.ts
@@ -6,42 +6,42 @@ import { ITransactionAttribute } from './attributes/Transaction.interface';
 
 @Entity('transactions')
 export class TransactionEntity extends BaseEntityIncludeTime implements ITransactionAttribute {
-    @Column()
+    @Column({ name: 'hash', type: 'character varying' })
     hash: string;
 
-    @Column()
+    @Column({ name: 'nonce', type: 'integer' })
     nonce: number;
 
-    @Column({ nullable: true })
+    @Column({ name: 'block_hash', type: 'character varying', nullable: true })
     blockHash?: string;
 
-    @Column({ nullable: true })
+    @Column({ name: 'block_number', type: 'integer', nullable: true })
     blockNumber?: number;
 
-    @Column({ nullable: true })
+    @Column({ name: 'transaction_index', type: 'integer', nullable: true })
     transactionIndex?: number;
 
-    @Column()
+    @Column({ name: 'from', type: 'character varying' })
     from: string;
 
-    @Column()
+    @Column({ name: 'to', type: 'character varying', nullable: true })
     to?: string;
 
-    @Column()
+    @Column({ name: 'value', type: 'character varying' })
     value: string;
 
-    @Column()
+    @Column({ name: 'gas_price', type: 'character varying' })
     gasPrice: string;
 
-    @Column({ nullable: true })
+    @Column({ name: 'max_priority_fee_per_gas', type: 'integer', nullable: true })
     maxPriorityFeePerGas?: number;
 
-    @Column({ nullable: true })
+    @Column({ name: 'max_fee_per_gas', type: 'integer', nullable: true })
     maxFeePerGas?: number;
 
-    @Column()
+    @Column({ name: 'gas', type: 'integer' })
     gas: number;
 
-    @Column()
+    @Column({ name: 'input', type: 'character varying' })
     input: string;
 }
