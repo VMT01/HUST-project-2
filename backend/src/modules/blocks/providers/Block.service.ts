83f415cd733fb9b834add5b7020cce22da5be7eb
Sotatek-TanHoang <tan.hoang@sotatek.com>
2022-06-04 20:12:16 +0700

    rename block api param

--- backend/src/modules/blocks/providers/Block.service.ts
+++ backend/src/modules/blocks/providers/Block.service.ts
@@ -10,21 +10,22 @@ export class BlockService {
     constructor(private readonly blockRepo: BlockRepository) {}
 
     async getBlocks(options: BlockQueryParams) {
+        
         const data = await this.blockRepo.getMany(options);
 
         return BasePaginationResponseDto.convertToPaginationResponse(data, options.page || 1);
     }
 
-    async getBlockByHashOrNumber(target: string) {
+    async getBlockByHashOrNumber(identifier: string) {
         const option: BlockQueryParams = {};
-        if (target.match(/^0x([A-Fa-f0-9]{64})$/)) {
-            // if hash is
-            option.hash = target;
-        } else if (target.match(/^[0-9]*$/)) {
-            option.number = parseInt(target);
+        if (identifier.match(/^0x([A-Fa-f0-9]{64})$/)) {// indentifier is hash
+            option.hash = identifier;
+        } else if (identifier.match(/^[0-9]*$/)) { // identifier is block number
+            option.number = parseInt(identifier);
         } else {
             throw new HttpException('Unknown block indentifier!', HttpStatus.BAD_REQUEST);
         }
+
         const block = await this.blockRepo.getOne(option);
 
         if (!block) throw new NotFoundException('Block not found');
