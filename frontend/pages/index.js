29ea11c34f145db88192540c6dc384d7516d544a
Vũ Minh Thanh <vuminhthanh8501@gmail.com>
2022-06-20 10:39:52 +0700

    feat: Create searchbar interface

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -5,7 +5,9 @@ import TransactionTable from "../components/TransactionTable";
 export default function Home() {
   return (
     <div>
-      <SearchBar />
+      <div style={{ width: "60%" }}>
+        <SearchBar />
+      </div>
       <div>
         <BlockTable />
         <TransactionTable />
