0ded66a3f0a656d117c970e5dc651adc93172a50
Vũ Minh Thanh <vuminhthanh8501@gmail.com>
2022-06-30 10:45:52 +0700

    feat: Create block detail component - done

--- frontend/components/block/BlockTxn.js
+++ frontend/components/block/BlockTxn.js
@@ -1,31 +1,47 @@
-import Web3 from "web3";
+import { useMemo } from "react";
 import style from "../../styles/blockDetail.module.scss";
+import Web3 from "web3";
 
 export default function BlockTxn({ data }) {
-  // const info = useMemo(
-  //   () =>
-  //     data.data.map(async (item) => {
-  //       const txn = await web3.eth.getTransactionReceipt(item.hash);
-
-  //       return {
-  //         hash: item.hash,
-  //         type: txn.type,
-  //         from: item.from,
-  //         to: item.to,
-  //         fee: web3.utils.fromWei(item.gas),
-  //       };
-  //     }),
-  //   // eslint-disable-next-line
-  //   []
-  // );
-
-  // console.log(info);
-
+  const info = useMemo(
+    () => {
+      const res = data.data.map((txn) => ({
+        hash: txn.hash,
+        method: txn.method,
+        from: txn.from,
+        to: txn.to,
+        fee: Web3.utils.fromWei(txn.maxFeePerGas * txn.gas + ""),
+      }));
+      return [
+        {
+          hash: "Txn Hash",
+          method: "Method",
+          from: "From",
+          to: "To",
+          fee: "Txn Fee",
+        },
+        ...res,
+      ];
+    },
+    //eslint-disable-next-line
+    []
+  );
   return (
     <div className={style["block-txn"]}>
-      <div>
-        <span>Transactions</span>
+      <div className={style["block-txn__title"]}>
+        <span className={style.type}>Transactions</span>
         <span className={style.title}>{data.total} Transactions</span>
+      </div>
+      <div className={style["block-txn__txn-list"]}>
+        {info.map((item, index) => (
+          <div key={index}>
+            <span>{item.hash}</span>
+            <span>{item.method}</span>
+            <span>{item.from}</span>
+            <span>{item.to}</span>
+            <span>{item.fee}</span>
+          </div>
+        ))}
       </div>
     </div>
   );
